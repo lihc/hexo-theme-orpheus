8f0fe6ee679b7ff42449f9e055badd2a655ac4fa
lihc <lihaicheng.email@gmail.com>
2023-11-19 20:42:47 +0800

    feat: 对侧边栏mobile-nav进行优化，实现可用

--- source/js/main.js
+++ source/js/main.js
@@ -1,25 +1,31 @@
-var navMenu = document.getElementById("nav-menu");
 var navButtonIn = document.getElementById("nav-button-in");
 var styleMobileSide = document.querySelector('.mobile-side');
+var navMenu = document.getElementById("nav-menu");
+var mobileNav = document.getElementById("mobile-nav");
+var windowNav = document.getElementById("window-nav");
 
 function checkWindowWidth() {
     var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
     if (windowWidth < 767) {
-        navMenu.style.display = "none";
+        // navMenu.style.display = "none";
         navButtonIn.style.removeProperty('display');
+        mobileNav.appendChild(navMenu);
     } else if (windowWidth >= 768 && windowWidth <= 1023) {
         var isPortrait = window.innerHeight > window.innerWidth;
         if (isPortrait) {
-            navMenu.style.display = "none";
+            // navMenu.style.display = "none";
             navButtonIn.style.removeProperty('display');
+            mobileNav.appendChild(navMenu);
         } else {
-            navMenu.style.removeProperty('display');
+            // navMenu.style.removeProperty('display');
             navButtonIn.style.display = "none";
+            windowNav.appendChild(navMenu);
         }
     } else if (windowWidth > 1024) {
-        navMenu.style.removeProperty('display');
+        // navMenu.style.removeProperty('display');
         navButtonIn.style.display = "none";
+        windowNav.appendChild(navMenu);
     }
 }
 
@@ -31,7 +37,6 @@ document.getElementById("nav-button-in").addEventListener("click", function(){
     var styleMobileSide = document.querySelector('.mobile-side');
     styleMobileSide.style.animation = 'r-slide-in 0.25s ease-out forwards';
     mobileSide.style.removeProperty('display');
-    console.log(mobileSide);
 });
 
 document.getElementById("nav-button-out").addEventListener("click", function(){
